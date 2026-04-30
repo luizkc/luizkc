a8255946221718f6b7d18f760142cb173252373d
luizkc <hi@luizkc.dev>
2026-04-30 14:59:28 -0400

    Fix viewport scroll on short pages

--- src/components/home/screen.tsx
+++ src/components/home/screen.tsx
@@ -21,7 +21,7 @@ export function HomeScreen({
 
   return (
     <motion.main
-      className="prose prose-zinc min-h-screen dark:prose-invert"
+      className="prose prose-zinc min-h-[calc(100dvh-4rem)] dark:prose-invert"
       variants={parentVariants}
       initial="hidden"
       animate="visible"
